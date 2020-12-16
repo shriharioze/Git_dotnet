564fef3d4f7fe2f4bfa444a567979980326a95ae
shriharioze <shriharioze07@gmail.com>
2020-12-16 09:36:40 +0530

    Updated

--- SampleProject1/Program.cs
+++ SampleProject1/Program.cs
@@ -11,6 +11,7 @@ namespace SampleProject1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
